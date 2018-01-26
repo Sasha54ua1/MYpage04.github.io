e6f1199b2b7a417d3d764a0a8eabd93dae1e6b39
Sasha54ua1 <Sasha54ua@gmail.com>
2018-01-26 15:25:39 +0200

    commit

---
+++ js/my.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var $container = $(".masonry-container");
+	$container.imagesLoaded(function () {
+		$container.masonry({
+			columnWidth: ".item",
+		itemSelector: ".item"
+        });
+     $(".item").imagefill();
+    });
+
+    
+    
+});
